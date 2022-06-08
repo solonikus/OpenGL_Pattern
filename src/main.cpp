3482faa05cb522d7349b2d2d9382286efa73b944
Michail <46868942+solonikus@users.noreply.github.com>
2022-06-09 00:28:47 +0300

    Init_repo
    Движок с кубом для дальнейших манипуляций

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "scene.h"
+
+int main()
+{
+	try
+	{
+		Scene main_scene;
+		main_scene.InitScene();
+		main_scene.Loop();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return 0;
+}
